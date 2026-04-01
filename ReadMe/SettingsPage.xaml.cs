6e19ed5aa432d9e2083d9d9916af80d769e59747
Damien Rochat <dami.scoot3@gmail.com>
2026-04-01 15:48:15 +0200

    Feat(DarkMode): ajout d'un dark mode
    [20][WPI]

--- ReadMe/SettingsPage.xaml.cs
+++ ReadMe/SettingsPage.xaml.cs
@@ -1,10 +1,21 @@
-namespace ReadMe
+namespace ReadMe;
+
+public partial class SettingsPage : ContentPage
 {
-    public partial class SettingsPage : ContentPage
+    public SettingsPage()
     {
-        public SettingsPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        // On synchronise le switch avec le thème actuel au chargement
+        ThemeSwitch.IsToggled = Application.Current.UserAppTheme == AppTheme.Dark;
+    }
+
+    private async void OnThemeToggled(object sender, ToggledEventArgs e)
+    {
+        // On change le thème
+        Application.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
+
+        // Optionnel : Enregistrer le choix dans les préférences du téléphone
+        Preferences.Default.Set("AppTheme", (int)Application.Current.UserAppTheme);
     }
 }
